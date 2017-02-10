96e0ff2dd9d5a56ce79a381778778d9ca1ae66b6
conanskyforce <conanskyforce@gmail.com>
2017-02-10 11:44:01 +0800

    util

---
+++ util.js
@@ -0,0 +1,110 @@
+var EventUitl = {
+	addHandler:function(element,type,handler){
+		if(element.addEventListener){
+			element.addEventListener(type,handler,false);
+		}else if(element.attachEvent){
+			element.attachEvent('on'+type,handler);
+		}
+		else{
+			element['on'+type] = handler;
+		}
+	},
+	getEvent:function(event){
+		return event?event:window.event;
+	},
+	getTarget:function(event){
+		return event.target||event.srcElement;
+	},
+	preventDefault:function(event){
+		if(event.preventDefault){
+			event.preventDefault();
+		}else{
+			event.returnVlaue = false;
+		}
+	},
+	removeHandler:function(element,type,handler){
+		if(element.removeEventListener){
+			element.removeEventListener(type,handler,false);
+		}else if(element.detachEvent){
+			element.detachEvent('on'+type,handler);
+		}else{
+			element['on'+type] = null;
+		}
+	},
+	stopPropagation:function(event){
+		if(event.stopPropagation){
+			event.stopPropagation();
+		}else{
+			event.cancelBubble = true;
+		}
+	},
+	getRelatedTarget:function(event){
+		if(event.relatedTarget){
+			return event.relatedTarget;
+		}else if(event.toElement){
+			return event.toElement;
+		}else if(event.fromElement){
+			return event.fromElement;
+		}else{
+			return null;
+		}
+	},
+	getButton:function(event){
+		if(document.implementation.hasFeature("MouseEvents","2.0")){
+			return event.button;
+		}else{
+			switch(event.button){
+				case 0:
+				case 1:
+				case 2:
+				case 3:
+				case 5:
+				case 7:
+					return 0;
+				case 2:
+				case 6:
+					return 2;
+				case 4:
+					return 1;
+			}
+		}
+	},
+	getWheelDelta:function(){
+		if(event.wheelDelta){
+			return (client.engine.opera&&client.engine.opera<9.5?
+				-event.wheelDelta:event.wheelDelta);
+		}else{
+			return -event.detail * 40;
+		}
+	},
+	getCharCode:function(event){
+		if(typeof event.charCode == "number"){
+			return event.charCode;
+		}else{
+			return event.keyCode;
+		}
+	}
+};
+
+var handler = function(event){
+	switch(event.type){
+		case 'click':
+			alert('clicked!');
+			break;
+		case 'mouseover':
+			event.target.style.backgroundColor = "red";
+			break;
+		case 'dbclick':
+			alert('dbclicked!');
+			break;
+		case 'mouseout':
+			event.target.style.backgroundColor = "";
+			break;
+	}
+}
+
+
+
+
+
+
